759956de6fb35765351bc81246cd831b653f72ae
YogurtFusion <aniketkumar33805@outlook.com>
2025-11-25 13:21:28 +0530

    setting up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ function Navbar() {
             <div className='logo px-8 mx-8 my-2'>
                 <img className='w-24  rounded-full' src="https://picsum.photos/500" alt="logo" />
             </div>
-            <ul className=' text-[16px]  flex justify-between items-center gap-8 mr-12' >
-                <li>Home</li>
-                <li>Library</li>
-                <li>Genre</li>
-                <li><img className='w-10 m-auto' src={profileIcon} alt="" /></li>
+            <ul className=' tracking-[150%] text-[20px]  flex justify-between items-center gap-8 mr-12' >
+                <li className='  hover:border-b hover:border-[#F5F5F5] ' >Home</li>
+                <li className='  hover:border-b hover:border-[#F5F5F5] ' >Library</li>
+                <li className='  hover:border-b hover:border-[#F5F5F5] ' >Genre</li>
+                <li><img className=' invert w-10 m-auto' src={profileIcon} alt="profileIcon" /></li>
             </ul>
         </nav>
     )
