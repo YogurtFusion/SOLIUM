65c1dfe41057714bcc349ddef569d64b991bb7a7
YogurtFusion <cocaniket2@gmail.com>
2026-03-12 17:02:04 +0530

    added the form in the form section

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import MainButton from "./UI/Button";
-
+const formData = [
+  { id: "film-name", title: "Film Name", type: "text" },
+//   { id: "release-date", title: "Release Date", type: "date", hidden: "hidden " },
+  { id: "director", title: "Director's Name", type: "text" },
+  { id: "genre", title: "Genre", type: "text" },
+  { id: "description", title: "Description", type: "textarea" },
+];
 const Form = () => {
   return (
     <section className="min-h-screen bg-neutral-950">
       {/* wrap */}
-      <div className=" grid grid-cols-2 justify-center items-center min-h-screen ">
+      <div className=" grid grid-cols-1 md:grid-cols-2 justify-center items-center min-h-screen ">
         <div className="flex justify-center items-center">
           <svg
             className="w-[60%] h-[60%]"
@@ -43,64 +49,44 @@ const Form = () => {
           </svg>
         </div>
         {/* FORM */}
-          <div className=" bg-neutral-900  h-fit p-12 w-fit mx-auto text-white  ">
-
-          <h2 className="text-center text-4xl font-Playfair tracking-tight leading-tight  ">
-            Hav a film in mind ?
+        <div className=" bg-neutral-900  h-fit py-6 md:p-12 w-fit mx-auto text-white  ">
+          <h2 className="text-center my-4 text-4xl font-Playfair tracking-tight leading-tight  ">
+            Have a film in mind ?
           </h2>
-          <div className="flex flex-col items-start justify-start gap-4 py-8 px-16">
-            <div className="flex flex-col gap-4 ">
-              <label
-                htmlFor="flim-name"
-                className="border border-neutral-500 hover:border-neutral-400 active:border-neutral-400 px-6 py-2"
+          <form className="flex flex-col items-start justify-start gap-4 py-8 px-16 ">
+            <div className="flex flex-col gap-4 w-full">
+              {formData.map((item) => (
+                <label
+                  key={item.id}
+                  htmlFor={item.id}
+                  className={`border text-base border-neutral-500 hover:border-neutral-400 focus-within:border-white active:border-neutral-400 px-6 py-2 bg-transparent transition-colors ${item.hidden} `}
                 >
-                <input
-                  type="text"
-                  className=" outline-none aria-selected:bg-neutral-50 "
-                  placeholder="Film Name"
-                  />
-              </label>
-              <label
-                htmlFor="flim-name"
-                className="border border-neutral-500 hover:border-neutral-400 active:border-neutral-400 px-6 py-2"
-                >
-                <input
-                  type="text"
-                  className=" outline-none aria-selected:bg-neutral-50 "
-                  placeholder="Film Name"
-                  />
-              </label>
-              <label
-                htmlFor="flim-name"
-                className="border border-neutral-500 hover:border-neutral-400 active:border-neutral-400 px-6 py-2"
-                >
-                <input
-                  type="text"
-                  className=" outline-none aria-selected:bg-neutral-50 "
-                  placeholder="Film Name"
-                  />
-              </label>
-              <label
-                htmlFor="flim-name"
-                className="border border-neutral-500 hover:border-neutral-400 active:border-neutral-400 px-6 py-2"
-                >
-                <input
-                  type="text"
-                  className=" outline-none aria-selected:bg-neutral-50 "
-                  placeholder="Film Name"
-                  />
-              </label>
-
+                  {item.type === "textarea" ? (
+                    <textarea
+                      id={item.id}
+                      htmlFor={item.id}
+                      placeholder={item.title}
+                      className=" outline-none bg-transparent w-full resize-none h-24 text-white  "
+                    />
+                  ) : (
+                    <input
+                      id={item.id}
+                      type={item.type}
+                      className={` outline-none bg-transparent w-full text-white ${item.hidden}`}
+                      placeholder={item.title}
+                    />
+                  )}
+                </label>
+              ))}
             </div>
             <MainButton
               title={"Submit"}
               px={"px-4"}
               py={"py-2"}
               hide={"hidden"}
-              />
-          </div>
+            />
+          </form>
         </div>
-           
       </div>
     </section>
   );
