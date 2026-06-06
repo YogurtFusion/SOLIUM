6b023b4300d797245addad2e977c51769a9c3867
YogurtFusion <cocaniket2@gmail.com>
2026-06-06 17:56:50 +0530

    fixed form

--- client/src/components/sections/Form.jsx
+++ client/src/components/sections/Form.jsx
@@ -12,62 +12,59 @@ const formData = [
 ];
 const Form = () => {
   return (
-    <section className="min-h-screen bg-neutral-950 selection:bg-white selection:text-black">
-      {/* wrap */}
-      <div className=" grid grid-cols-1  md:grid-cols-2 justify-center items-center min-h-screen  ">
-        <div className="md:flex justify-center items-center hidden   ">
-          <video width={640} height={360} 
-            autoPlay 
-            muted 
-            loop 
-            playsInline
-            preload="auto"
-            onLoadedMetadata={(e)=>e.target.plabackRate=3}
-            >
-
-            <source src={SphereVideo} type="video/mp4" />
-          </video>
-          {/* <ShpereMotion/> */}
-        </div>
-        {/* FORM */}
-        <div className=" bg-neutral-900  h-fit py-6 md:p-12 w-full max-w-138 mx-auto text-white  ">
-          <h2 className="text-center my-4 text-4xl font-Playfair tracking-tight leading-tight  ">
+    <section className="min-h-screen bg-neutral-950 selection:bg-white selection:text-black text-white">
+      <div className="h-fit p-12 md:p-24 w-full max-w-7xl mx-auto flex flex-col md:flex-row gap-16 lg:gap-24">
+        <div className=" max-w-sm text-left">
+          <h2 className=" my-4 text-5xl md:text-6xl lg:text-7xl font-Playfair tracking-tight leading-tight ">
             Got a film to share?
           </h2>
-          <form className="flex flex-col items-start justify-start gap-4 py-8 px-16 ">
-            <div className="flex flex-col gap-4 w-full">
-              {formData.map((item) => (
-                <label
-                  key={item.id}
-                  htmlFor={item.id}
-                  className={`border text-base border-neutral-500 hover:border-neutral-400 focus-within:border-white active:border-neutral-400 px-6 py-2 bg-transparent transition-colors ${item.hidden} `}
-                >
-                  {item.type === "textarea" ? (
-                    <textarea
-                      id={item.id}
-                      htmlFor={item.id}
-                      placeholder={item.title}
-                      className=" outline-none bg-transparent w-full resize-none h-24 text-white  "
-                    />
-                  ) : (
-                    <input
-                      id={item.id}
-                      type={item.type}
-                      className={` outline-none bg-transparent w-full text-white ${item.hidden}`}
-                      placeholder={item.title}
-                    />
-                  )}
-                </label>
-              ))}
-            </div>
-            <MainButton
-              title={"Submit"}
-              px={"px-4"}
-              py={"py-2"}
-              hide={"hidden"}
-            />
-          </form>
+          <p className="text-neutral-600 md:text-base text-sm leading-relaxed text-pretty ">
+            Submit your work for consideration in our curated catalog. We are
+            always looking for unique voices, striking visuals, and deeply human
+            stories.
+          </p>
         </div>
+        <form className="flex flex-col items-start justify-start gap-4 lg:py-8 lg:px-16 w-full font-poppins">
+          <div className="flex flex-col  gap-12 w-full">
+            {formData.map((item) => (
+              <div
+                key={item.id}
+                className={`relative border-b text-base border-neutral-800 hover:border-neutral-600  active:border-neutral-400 focus-within:border-white   bg-transparent transition-colors ${item.hidden||""} flex flex-col items-start`}
+              >
+               
+                {item.type === "textarea" ? (
+                  <textarea
+                    id={item.id}
+                    name={item.id}
+                    placeholder=""
+                    className="peer outline-none bg-transparent w-full resize-none h-24 pt-6 pb-2 text-neutral-300 transition-colors "
+                  />
+                ) : (
+                  <input
+                    id={item.id}
+                    name={item.id}
+                    autoComplete="off"
+                    type={item.type}
+                    className={`peer outline-none bg-transparent w-full pt-6 pb-2 text-neutral-300 transition-colors ${item.hidden||""}`}
+                    placeholder=""
+                  />
+                )}
+                 <label
+                  htmlFor={item.id}
+                  className="absolute left-0 top-6 text-neutral-500  pointer-events-none transition-all duration-300 ease-out origin-left peer-focus:top-0 peer-focus:scale-75 peer-focus:text-white peer-[:not(:placeholder-shown)]:top-0 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:text-neutral-400 "
+                >
+                  {item.title}
+                </label>
+              </div>
+            ))}
+          </div>
+          <MainButton
+            title={"Submit"}
+            px={"px-4"}
+            py={"py-2"}
+            hide={"hidden"}
+          />
+        </form>
       </div>
     </section>
   );
